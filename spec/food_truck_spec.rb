7daf4eb70d68f5f3465f437fbdd5e98737a70406
Cory Bretsch <corybretsch@gmail.com>
2024-04-15 10:37:13 -0500

    iteration 1 tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+require './lib/item'
 require 'pry'
 
 RSpec.configure do |config|
@@ -7,15 +8,64 @@ end
 
 RSpec.describe FoodTruck do
   it "can initialize" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    expect(food_truck).to be_a(FoodTruck)
   end
 
+  it "has a name" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+  
+  it "has an inventory default empty hash" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    expect(food_truck.inventory).to eq({})
+  end
+  
+  it "can check stock of an item with default 0" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
+    expect(food_truck.check_stock(item1)).to eq 0
+  end
+  
+  it "can stock an item" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
 
+    expect(food_truck.inventory).to eq({item1 => 30})
+  end
+  
+  it "can check stock of an item" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
 
+    expect(food_truck.check_stock(item1)).to be 30
+  end
+  
+  it "can stock an item" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
 
+    expect(food_truck.check_stock(item1)).to be 55
+  end
+  
+  it "can store inventory as key/value pairs" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
 
+    expect(food_truck.inventory).to eq([item1 => 55, item2 => 12])
+  end
 
 end
